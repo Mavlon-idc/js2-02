4e0fd513dd3c9da346bf8ddddece0fdc4a2dee93
Mavlon <mavlon.idc@mail.ru>
2020-11-23 20:58:40 +0500

    js-02

---
+++ script.js
@@ -0,0 +1,76 @@
+var time = +prompt('Введите время от 0 до 24')
+if(time == 24){
+    alert('12 часов ночи')
+}
+    else if(time == 1){
+        alert('1 час ночи')
+    }
+        else if(time == 2){
+            alert('2 часа ночи')
+        }
+            else if(time == 3){
+                alert('3 часа ночи')
+            }
+                else if(time == 4){
+                    alert('4 часа ночи')
+                }   
+                    else if(time == 5){
+                        alert('5 часов ночи')
+                    }   
+                       else if(time == 6){
+                        alert('6 часов ночи')
+                        }   
+                        else if(time == 7){
+                            alert('7 утра')
+                        }
+                            else if(time == 8){
+                                alert('8 утра')
+                            }
+                                else if(time == 9){
+                                    alert('9 утра')
+                                }
+                                    else if(time == 10){
+                                        alert('10 утра')
+                                    }   
+                                        else if(time == 11){
+                                            alert('11 утра')
+                                        }   
+                                           else if(time == 12){
+                                            alert('12 дня')
+                                            }
+                                            else if(time == 13){
+                                                alert('час дня')
+                                            }
+                                                else if(time == 14){
+                                                    alert('2 часа дня')
+                                                }
+                                                    else if(time == 15){
+                                                        alert('3 часа дня')
+                                                    }
+                                                        else if(time == 16){
+                                                            alert('4 часа дня')
+                                                        }   
+                                                            else if(time == 17){
+                                                                alert('5 часов вечера')
+                                                            }   
+                                                               else if(time == 18){
+                                                                alert('6 часов вечера')
+                                                                }
+                                                                else if(time == 19){
+                                                                    alert('7 часов вечера')
+                                                                    }
+                                                                    else if(time == 20){
+                                                                        alert('8 часов вечера')
+                                                                    }
+                                                                        else if(time == 21){
+                                                                            alert('9 часов вечера')
+                                                                        }
+                                                                            else if(time == 22){
+                                                                                alert('10 часов вечера')
+                                                                            }
+                                                                                else if(time == 23){
+                                                                                    alert('11 часов ночи')
+                                                                                }   
+                                                                                    else{
+                                                                                        alert('Ты дурак?')
+                                                                                    }
